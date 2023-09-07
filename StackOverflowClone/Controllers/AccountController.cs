9f13a6fbbecf81bb2c1ab5a6bacbbe9b8ca39801
Tusher Ahmed <ahmedshad1997@gmail.com>
2023-09-08 05:56:37 +0600

    Client Model Added Using NHibernate

---
+++ StackOverflowClone/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using NHibernate;
+using StackOverflowClone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StackOverflowClone.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            IList<Client> users;
+            using (ISession session = NHibernateSession.OpenSession())
+            {
+                users = session.Query<Client>().ToList();
+            }
+            return View(users);
+        }
+        public ActionResult Details(int id)
+        {
+            Client user= new Client();
+            using (ISession session = NHibernateSession.OpenSession())
+            {
+                user = session.Query<Client>().Where(b => b.Id == id).FirstOrDefault();
+            }
+
+            return View(user);
+        }
+    }
+}
---
+++ StackOverflowClone/Models/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StackOverflowClone.Models
+{
+    public class Client
+    {
+
+        public virtual long Id { get; set; }
+        public virtual string Username { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string AboutMe { get; set; }
+        public virtual string Password { get; set; }
+    
+    }
+}
